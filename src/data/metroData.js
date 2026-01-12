519501ad3e6b74b2545235c0fdf83c88c1661b81
Shashank Harwalkar <shashank261@gmail.com>
2026-01-12 09:47:54 +0200

    Update sources

--- src/data/metroData.js
+++ src/data/metroData.js
@@ -3,154 +3,154 @@ export const metroData = [
     city: "Delhi-NCR",
     operational: 435.42,
     underConstruction: 154.65,
-    sourceName: "DMRC",
-    sourceUrl: "https://www.delhimetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Delhi_Metro"
   },
   {
     city: "Bengaluru",
     operational: 96.10,
     underConstruction: 143.43,
-    sourceName: "BMRCL",
-    sourceUrl: "https://english.bmrc.co.in/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Namma_Metro"
   },
   {
     city: "Mumbai",
     operational: 88.86,
     underConstruction: 176.05,
-    sourceName: "MMRCL / MMMOCL",
-    sourceUrl: "https://mmrcl.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Mumbai_Metro"
   },
   {
     city: "Kolkata",
     operational: 73.42,
     underConstruction: 75.20,
-    sourceName: "KMRC",
-    sourceUrl: "https://www.kmrc.in/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Kolkata_Metro"
   },
   {
     city: "Hyderabad",
     operational: 69.00,
     underConstruction: 0,
-    sourceName: "HMR",
-    sourceUrl: "https://www.ltmetro.in/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Hyderabad_Metro"
   },
   {
     city: "Chennai",
     operational: 54.25,
     underConstruction: 118.90,
-    sourceName: "CMRL",
-    sourceUrl: "https://chennaimetrorail.org/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Chennai_Metro"
   },
   {
     city: "Ahmedabad",
     operational: 45.45,
     underConstruction: 8.88,
-    sourceName: "GMRC",
-    sourceUrl: "https://www.gujaratmetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Ahmedabad_Metro"
   },
   {
     city: "Nagpur",
     operational: 40.02,
     underConstruction: 43.80,
-    sourceName: "Maha Metro",
-    sourceUrl: "https://www.metrorailnagpur.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Nagpur_Metro"
   },
   {
     city: "Pune",
     operational: 33.23,
     underConstruction: 33.13,
-    sourceName: "Pune Metro",
-    sourceUrl: "https://www.punemetrorail.org/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Pune_Metro"
   },
   {
     city: "Kochi",
     operational: 28.48,
     underConstruction: 11.20,
-    sourceName: "KMRL",
-    sourceUrl: "https://kochimetro.org/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Kochi_Metro"
   },
   {
     city: "Lucknow",
     operational: 22.88,
     underConstruction: 0,
-    sourceName: "UPMRC",
-    sourceUrl: "https://www.lmrcl.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Lucknow_Metro"
   },
   {
     city: "Gandhinagar",
     operational: 18.80,
     underConstruction: 0,
-    sourceName: "GMRC",
-    sourceUrl: "https://www.gujaratmetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Gandhinagar_Metro"
   },
   {
     city: "Kanpur",
     operational: 15.00,
     underConstruction: 23.66,
-    sourceName: "UPMRC",
-    sourceUrl: "https://www.lmrcl.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Kanpur_Metro"
   },
   {
     city: "Jaipur",
     operational: 12.10,
     underConstruction: 0,
-    sourceName: "JMRC",
-    sourceUrl: "https://transport.rajasthan.gov.in/jmrc"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Jaipur_Metro"
   },
   {
     city: "Gurgaon (Rapid)",
     operational: 12.10,
     underConstruction: 0,
-    sourceName: "HMRTC",
-    sourceUrl: "https://www.hmrtc.org.in/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Rapid_Metro_Gurgaon"
   },
   {
     city: "Navi Mumbai",
     operational: 11.10,
     underConstruction: 0,
-    sourceName: "CIDCO",
-    sourceUrl: "https://cidco.maharashtra.gov.in/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Navi_Mumbai_Metro"
   },
   {
     city: "Agra",
     operational: 6.00,
     underConstruction: 24.10,
-    sourceName: "UPMRC",
-    sourceUrl: "https://www.lmrcl.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Agra_Metro"
   },
   {
     city: "Surat",
     operational: 0,
     underConstruction: 40.35,
-    sourceName: "GMRC",
-    sourceUrl: "https://www.gujaratmetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Surat_Metro"
   },
   {
     city: "Patna",
     operational: 0,
     underConstruction: 32.51,
-    sourceName: "PMRC",
-    sourceUrl: "https://state.bihar.gov.in/urban/CitizenHome.html"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Patna_Metro"
   },
   {
     city: "Indore",
     operational: 0,
     underConstruction: 31.55,
-    sourceName: "MP Metro",
-    sourceUrl: "https://www.mpmetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Indore_Metro"
   },
   {
     city: "Thane",
     operational: 0,
     underConstruction: 29.00,
-    sourceName: "Maha Metro",
-    sourceUrl: "https://mahametro.org/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Thane_Metro"
   },
   {
     city: "Bhopal",
     operational: 0,
     underConstruction: 27.87,
-    sourceName: "MP Metro",
-    sourceUrl: "https://www.mpmetrorail.com/"
+    sourceName: "Wikipedia",
+    sourceUrl: "https://en.wikipedia.org/wiki/Bhopal_Metro"
   },
 ];
