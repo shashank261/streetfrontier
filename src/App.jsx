fade57943787d972572adc1f0f01011d5071de69
Shashank <shashank261@gmail.com>
2026-01-14 15:48:06 +0200

    Add vercel analytics (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
 import Home from './pages/Home';
 import MetroTracker from './pages/MetroTracker';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/metro-tracker" element={<MetroTracker />} />
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/metro-tracker" element={<MetroTracker />} />
+                </Routes>
+            </Router>
+            <Analytics />
+        </>
     );
 }
 
